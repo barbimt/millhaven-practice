a2e3be669a16b16ddb178356b83998ec5ad0dee0
Barbi Torres <barbi.mtorres@gmail.com>
2026-06-15 16:49:49 +0100

    fix: use Tailwind z-100 utility in skip link
    Replace arbitrary focus:z-[100] with the theme z-index class to satisfy the linter.

--- app/components/SkipToContent.vue
+++ app/components/SkipToContent.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     href="#main-content"
-    class="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[100] focus:rounded-lg focus:bg-white focus:px-4 focus:py-2 focus:font-josefin focus:text-sm focus:font-semibold focus:text-[#2A2B2B] focus:shadow-md focus:outline-2 focus:outline-offset-2 focus:outline-brand-purple"
+    class="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-100 focus:rounded-lg focus:bg-white focus:px-4 focus:py-2 focus:font-josefin focus:text-sm focus:font-semibold focus:text-[#2A2B2B] focus:shadow-md focus:outline-2 focus:outline-offset-2 focus:outline-brand-purple"
   >
     Skip to main content
   </a>
